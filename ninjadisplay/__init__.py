1400a13acf548d3560323c79bcc52738964c546d
Abdullah Khan <abdullah@ajkhan.me>
2020-01-12 17:57:54 -0500

    Add app

---
+++ ninjadisplay/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import render_template
+
+from .api import create_client
+from . import utils
+
+api = create_client()
+app = Flask(__name__)
+
+
+@app.route('/')
+def slash():
+    return render_template('base.html')
+
+
+@app.route('/alerts')
+def alerts():
+    all_alerts_info = api.get_alerts()
+    alerts = []
+    for device in all_alerts_info:
+        device_id = utils.safe_get(device, 'device', 'id')
+        customer_name = utils.safe_get(device, 'customer', 'name')
+        alerts.append(
+            utils.Alert(
+                hostname=utils.safe_get(device, 'device', 'system_name'),
+                customer=customer_name,
+                url=f'https://app.ninjarmm.com/#/deviceDashboard/{device_id}/overview',
+                alert=utils.safe_get(device, 'message')
+            )
+        )
+    return render_template('alerts.html', alerts=alerts)
