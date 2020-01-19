e05d2337c0fb81a8be7ab8a0c48816018d396fef
Abdullah Khan <ajkhan4096@gmail.com>
2020-01-19 00:48:42 -0500

    Edit __init__.py
    Uses absolute imports and now separates views into its own module.

--- ninjadisplay/__init__.py
+++ ninjadisplay/__init__.py
@@ -1,30 +1,9 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
-from .api import create_client
-from . import utils
-
-api = create_client()
 app = Flask(__name__)
-
 
 @app.route('/')
 def slash():
     return render_template('base.html')
 
-
-@app.route('/alerts')
-def alerts():
-    all_alerts_info = api.get_alerts()
-    alerts = []
-    for device in all_alerts_info:
-        device_id = utils.safe_get(device, 'device', 'id')
-        alerts.append(
-            utils.Alert(
-                hostname=utils.safe_get(device, 'device', 'system_name'),
-                customer=utils.safe_get(device, 'customer', 'name'),
-                url=f'https://app.ninjarmm.com/#/deviceDashboard/{device_id}/overview',
-                alert=utils.safe_get(device, 'message')
-            )
-        )
-    return render_template('alerts.html', alerts=alerts)
+from ninjadisplay import views
